f7b54880ef43d802330fcd85e864f4cc62dae8fc
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-05-25 20:23:05 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,5 +6,9 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     self.where("rating = #{self.highest_rating}").first
   end
+  
+  def self.lowest_rating
+    self.minimum("rating")
+  end
 end
 
